a40fc5d35514a38437a66c3d5896d41a89957afb
yuvraj5657663 <yuvrajkumar4588@gmail.com>
2023-09-07 03:16:37 +0530

    initial-commit

---
+++ variable.js
@@ -0,0 +1,12 @@
+const accountId = 2202030400212 
+let emailId = "yuvrajkumar4588@gmail.com"
+var accountPassword = "2345"
+accountCity = "bihar"
+let accountState;
+
+emailId = "yk811211@gmail.com"
+accountPassword = "73973"
+accountCity = "ahemdabad"
+
+console.log(accountId);
+console.table([ accountId,emailId, accountPassword, accountCity, accountState])
